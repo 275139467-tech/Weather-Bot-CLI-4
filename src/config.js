4c93650f59c83c37725e5474134ca07a88e42348
Peng Aik Goh <peng.goh@bytedance.com>
2026-05-18 09:10:00 +0800

    Use env variables

--- src/config.js
+++ src/config.js
@@ -1,8 +1,16 @@
 import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export function loadConfig(path = 'config.json') {
-  if (!fs.existsSync(path)) {
-    return {};
-  }
-  return JSON.parse(fs.readFileSync(path, 'utf8'));
+  const config = fs.existsSync(path) ? JSON.parse(fs.readFileSync(path, 'utf8')) : {};
+  return {
+    ...config,
+    weather_api_key: process.env.WEATHER_API_KEY,
+    notifications: {
+      ...(config.notifications ?? {}),
+      bot_token: process.env.NOTIFICATION_BOT_TOKEN
+    }
+  };
 }
